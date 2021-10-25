94e7dcdd227066af83a934d35a013bede381750f
Jonhny <jonhnywillian.jw@gmail.com>
2021-10-24 21:30:11 -0300

    24/10

---
+++ SistemaBarbearia/Models/ContaReceber/ContaReceber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaBarbearia.Models.ContaReceber
+{
+    public class ContaReceber
+    {
+
+        public ViewModels.FormaPagamentos.SelectFormaPagamentoVM FormaPag { get; set; }
+        public ViewModels.Clientes.SelectClienteVM Cliente { get; set; }
+
+        [Display(Name = "Nº parcela")]
+        public short nrParcela { get; set; }
+
+        [Display(Name = "Valor da parcela")]
+        public decimal vlParcela { get; set; }
+
+        [Display(Name = "Data de vencimento")]
+        public DateTime dtVencimento { get; set; }
+
+        [Display(Name = "Situação")]
+        public string flSituacao { get; set; }
+
+        [Display(Name = "Data de pagamento")]
+        public DateTime? dtPagamento { get; set; }
+    }
+}
---
+++ SistemaBarbearia/ViewModels/ContasReceber/ContaReceberVW.cs
@@ -0,0 +1,39 @@
+﻿using SistemaBarbearia.ViewModels.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaBarbearia.ViewModels.ContasReceber
+{
+    public class ContaReceberVW : ModelPaiVM
+    {
+        public ViewModels.FormaPagamentos.SelectFormaPagamentoVM FormaPag { get; set; }
+        public ViewModels.Clientes.SelectClienteVM Cliente { get; set; }
+
+
+        [Display(Name = "Nº Parcela")]
+        [StringLength(50, MinimumLength = 3)]
+        [Required(ErrorMessage = "Campo Modelo não Pode ser em Branco!")]
+        public int nrParcela { get; set; }
+
+        [Display(Name = "Valor Parcelas")]
+        [StringLength(50, MinimumLength = 3)]
+        [Required(ErrorMessage = "Campo Modelo não Pode ser em Branco!")]
+        public decimal? vlParcela { get; set; }
+
+        [Display(Name = "Dt. Vencimento")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato inválido")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? dtVencimento { get; set; }
+
+        [Display(Name = "Situação")]
+        public string flSituacao { get; set; }
+
+        [Display(Name = "Dt. Pagamento")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato inválido")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? dtPagamento { get; set; }
+    }
+}
